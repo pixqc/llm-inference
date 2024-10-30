1663103d6aa0d3313045b098fb3ac25059b34e27
pix <pixqc1159@gmail.com>
2024-10-30 12:33:30 +0000

    make script actually work

--- runpod_script.sh
+++ runpod_script.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 set -e
+export HUGGINGFACE_TOKEN=""
+if [ -z "$HUGGINGFACE_TOKEN" ]; then
+    echo "Error: HUGGINGFACE_TOKEN must not be empty."
+    exit 1
+fi
+
 pip install --upgrade torch
 pip install -U \
     tiktoken \
-    "jax[cuda12]" \
     "huggingface_hub[cli]" \
     transformers \
     blobfile
 
-HUGGING_FACE_TOKEN=""
-huggingface-cli login --token $HUGGING_FACE_TOKEN
+PYTHONPATH='.' MODEL_ID=meta-llama/Llama-3.2-1B OUT_DIR=src/model/1B python3 src/download_model.py
+PYTHONPATH='.' MODEL_ID=meta-llama/Llama-3.2-1B-Instruct OUT_DIR=src/model/1B-Instruct python3 src/download_model.py
 
-PYTHONPATH='.' MODEL_ID=meta-llama/Llama-3.2-1B OUT_DIR=src/model/1B python3 src/model/download_model.py
-PYTHONPATH='.' MODEL_ID=meta-llama/Llama-3.2-1B-Instruct OUT_DIR=src/model/1B-Instruct python3 src/model/download_model.py
-PYTHONPATH='.' python3 evals/download_evals.py
+mkdir -p evals/mmlu
+curl -L https://people.eecs.berkeley.edu/~hendrycks/data.tar -o /tmp/data.tar
+tar xf /tmp/data.tar -C /tmp
+mv /tmp/data/* evals/mmlu/
+rm -rf /tmp/data
+
+mkdir -p evals/math
+curl -L https://people.eecs.berkeley.edu/~hendrycks/MATH.tar -o /tmp/MATH.tar
+tar xf /tmp/MATH.tar -C /tmp
+mv /tmp/MATH/* evals/math/
+rm -rf /tmp/MATH
