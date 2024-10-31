f8f9236377453dc90bd8e6deb596323fd8d9c7bd
pix <pixqc1159@gmail.com>
2024-10-31 07:21:40 +0000

    wip fewshot mmlu

--- evals/eval_mmlu.py
+++ evals/eval_mmlu.py
@@ -1,7 +1,7 @@
 import csv
 import json
 from pathlib import Path
-from typing import List, NamedTuple
+from typing import Iterator, List, NamedTuple, Optional
 
 from src.torch_main import LLAMA_1B_PARAMS, Llama
 
@@ -11,47 +11,47 @@ class MMLU(NamedTuple):
   choices: List[str]
   answer: int
 
+  def format(self) -> tuple[str, str]:
+    prompt = (
+      "Given the following question and four candidate answers (A, B, C and D), choose the best answer."
+      f"\nQuestion: {self.question}\n"
+      + "\n".join(f"{c}. {a}" for c, a in zip("ABCD", self.choices))
+      + '\nYour response should end with "The best answer is [the_answer_letter]" where the [the_answer_letter] is one of A, B, C or D.'
+    )
+    return prompt, chr(self.answer + ord("A"))
 
-def read_mmlu(directory: Path) -> List[MMLU]:
-  return [
+
+def load_mmlu(path: Path) -> Iterator[MMLU]:
+  return (
     MMLU(row[0], row[1:5], ord(row[5]) - ord("A"))
-    for csv_file in directory.glob("*.csv")
+    for csv_file in path.glob("*.csv")
     for row in csv.reader(open(csv_file))
     if len(row) == 6
-  ]
-
-
-def format_q(mmlu: MMLU) -> str:
-  return (
-    "Given the following question and four candidate answers (A, B, C and D), choose the best answer."
-    f"\nQuestion: {mmlu.question}\n"
-    + "\n".join(f"{c}. {a}" for c, a in zip("ABCD", mmlu.choices))
-    + '\nYour response should end with "The best answer is [the_answer_letter]" where the [the_answer_letter] is one of A, B, C or D.'
   )
 
 
-def format_qa(mmlu: MMLU) -> tuple[str, str]:
-  return (format_q(mmlu), chr(mmlu.answer + ord("A")))
+def format_fewshot(question: str, examples: Optional[List[tuple[str, str]]]) -> str:
+  if examples:
+    examples_text = "\n\n".join(f"{q}\nThe best answer is {a}." for q, a in examples)
+    return f"{examples_text}\n\n{question}"
+  return question
 
 
 if __name__ == "__main__":
-  MMLU_DIR = Path("evals/mmlu/test")
-  mmlu = read_mmlu(MMLU_DIR)
-  mmlu_zero = [format_qa(data) for data in mmlu]
+  fewshot = [ex.format() for ex in list(load_mmlu(Path("evals/mmlu/val")))[:2]]
+  mmlu = [ex.format() for ex in load_mmlu(Path("evals/mmlu/test"))]
 
   is_instruct = True
-  weight_path, tok_path = "src/model/1B", "src/tokenizer.model"
-  weight_path = weight_path + "-Instruct" if is_instruct else weight_path
-  llama = Llama(is_instruct, LLAMA_1B_PARAMS, weight_path, tok_path)
+  weight_path = f"src/model/1B{'-Instruct' if is_instruct else ''}"
+  llama = Llama(is_instruct, LLAMA_1B_PARAMS, weight_path, "src/tokenizer.model")
 
-  correct, total = 0, 0
+  correct = total = 0
   with open("evals/eval_mmlu.jsonl", "a") as f:
-    for question, answer in mmlu_zero:
+    for question, answer in mmlu:
       total += 1
+      prompt = format_fewshot(question, fewshot)
       tokens, attn_mask = llama.tokenize(
-        [question],
-        format_instruct=True,
-        postfix="The best answer is",
+        [prompt], format_instruct=True, postfix="The best answer is"
       )
       print(llama.detokenize(tokens[0]), end="")
       for chunk in llama.generate(
@@ -59,19 +59,16 @@ if __name__ == "__main__":
       ):
         pred = chunk["choices"][0]["delta"]["content"].strip()
         print(f"({pred},{answer})", end="", flush=True)
-        logprobs = chunk["top_predictions"]
-        logprobs = [{"token": tp["token"], "logprob": tp["logprob"]} for tp in logprobs]
         result = {
-          "question": question,
+          "prompt": prompt,
           "pred": pred,
-          "ground_truth": answer,
-          "logprobs": logprobs,
+          "answer": answer,
+          "logprobs": [
+            {"token": tp["token"], "logprob": tp["logprob"]}
+            for tp in chunk["top_predictions"]
+          ],
         }
-        if pred == answer:
-          correct += 1
-        f.write(json.dumps(result) + "\n")
-        f.flush()
+        correct += pred == answer
+        f.write(f"{json.dumps(result)}\n")
         break
-
-  accuracy = correct / total
-  print(f"\nMMLU Accuracy: {accuracy:.2%}")
+  print(f"\nMMLU Accuracy: {correct/total:.2%}")
